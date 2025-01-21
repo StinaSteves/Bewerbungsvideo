cdcc3da8b92be60c1d56acf82abd671bc6f9fa96
Stina Steves <stinasteves@mac.speedport.ip>
2025-01-21 21:07:00 +0100

    video lfs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-lfs-files',
+      closeBundle() {
+        copyFileSync('Videos/Bewerbungsvideo.mp4', 'dist/Videos/Bewerbungsvideo.mp4');
+      },
+    },
+  ],
   base: '/Bewerbungsvideo/', // Ersetze <repository-name> durch den Namen deines Repositories
 });
