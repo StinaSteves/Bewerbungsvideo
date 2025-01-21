dcf968bc97bd212a13a016a0e9222af38afd9b9d
Stina Steves <stinasteves@mac.speedport.ip>
2025-01-21 20:44:26 +0100

    images

--- src/Components/NaturalFizz/Home.jsx
+++ src/Components/NaturalFizz/Home.jsx
@@ -8,39 +8,39 @@ const mapFlavors = [
   {
     name: "Lemon",
     fruitCan: "/Bewerbungsvideo/public/images/NaturalFizz/lemonCan.png",
-    fruit: "/images/NaturalFizz/lemon.png",
+    fruit: "/Bewerbungsvideo/public/images/NaturalFizz/lemon.png",
     backgroundColor: "#f4d453",
     flavorDescription:
       "The organic lemon soda tingles refreshingly on the tongue, zesty acidity from freshly squeezed lemons.",
   },
   {
     name: "Apple",
-    fruitCan: "/Bewerbungsvideo/images/NaturalFizz/appleCan.png",
-    fruit: "/images/NaturalFizz/apple.png",
+    fruitCan: "/Bewerbungsvideo/public/images/NaturalFizz/appleCan.png",
+    fruit: "/Bewerbungsvideo/public/images/NaturalFizz/apple.png",
     backgroundColor: "#ea3f48",
     flavorDescription:
       "The organic apple soda is crisp and refreshing, with a burst of sweet, juicy apples.",
   },
   {
     name: "Blueberry",
-    fruitCan: "/images/NaturalFizz/blueberryCan.png",
-    fruit: "/images/NaturalFizz/blueberry.png",
+    fruitCan: "/Bewerbungsvideo/public/images/NaturalFizz/blueberryCan.png",
+    fruit: "/Bewerbungsvideo/public/images/NaturalFizz/blueberry.png",
     backgroundColor: "#8b90ae",
     flavorDescription:
       "The organic blueberry soda is vibrant and tangy, with a burst of sweet, wild blueberries.",
   },
   {
     name: "Peach",
-    fruitCan: "/images/NaturalFizz/peachCan.png",
-    fruit: "/images/NaturalFizz/peach.png",
+    fruitCan: "/Bewerbungsvideo/public/images/NaturalFizz/peachCan.png",
+    fruit: "/Bewerbungsvideo/public/images/NaturalFizz/peach.png",
     backgroundColor: "#f9af3c",
     flavorDescription:
       "The organic peach soda is sweet and juicy, with a smooth, mellow flavor of ripe peaches.",
   },
   {
     name: "Unlocking the Power",
-    fruitCan: "/images/NaturalFizz/peachCan.png",
-    fruit: "/images/NaturalFizz/waterDrops.png",
+    fruitCan: "/Bewerbungsvideo/public/images/NaturalFizz/peachCan.png",
+    fruit: "/Bewerbungsvideo/public/images/NaturalFizz/waterDrops.png",
     backgroundColor: "#f9af3c",
     flavorDescription:
       "BUY NATURALFIZZ NOW!",
